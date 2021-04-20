028fbe802099db4cc4a9437f9ebe163b41e475ac
Alex <alexislafrances-laf@outlook.fr>
2021-04-20 11:22:37 +0200

    fix some bugs : leaks, cast to int

--- srcs/push_swap/algo_sort_3.c
+++ srcs/push_swap/algo_sort_3.c
@@ -6,7 +6,7 @@
 /*   By: alafranc <alafranc@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 17:13:22 by alafranc          #+#    #+#             */
-/*   Updated: 2021/04/08 13:06:11 by alafranc         ###   ########lyon.fr   */
+/*   Updated: 2021/04/20 10:35:50 by alafranc         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 void	algo_sort_3(int count, t_list **lst)
 {
-	if (count == 1)
-		return ;
 	if (count == 2)
 	{
 		if ((*lst)->content > (*lst)->next->content)
@@ -23,12 +21,12 @@ void	algo_sort_3(int count, t_list **lst)
 	}
 	if (count == 3)
 	{	
-		if ((*lst)->content > ft_lstlast(*lst)->content
-			&& (*lst)->content > (*lst)->next->content)
+		if ((int)(*lst)->content > (int)ft_lstlast(*lst)->content
+			&& (int)(*lst)->content > (int)(*lst)->next->content)
 			rotate_a(lst, NULL, 1);
-		if (ft_lstlast(*lst)->content < (*lst)->next->content)
+		if ((int)ft_lstlast(*lst)->content < (int)(*lst)->next->content)
 			reverse_rotate_a(lst, NULL, 1);
-		if ((*lst)->content > (*lst)->next->content)
+		if ((int)(*lst)->content > (int)(*lst)->next->content)
 			swap_a(lst, NULL, 1);
 	}
 }
